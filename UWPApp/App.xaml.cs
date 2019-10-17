c39120a5074ca0f0ab886c1144758b904637b57a
Kazuki Ota <kaota@microsoft.com>
2019-10-17 22:16:59 +0900

    add xaml islands

---
+++ UWPApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Toolkit.Win32.UI.XamlHost;
+
+namespace UWPApp
+{
+    sealed partial class App : XamlApplication
+    {
+        public App()
+        {
+            Initialize();
+        }
+    }
+}
