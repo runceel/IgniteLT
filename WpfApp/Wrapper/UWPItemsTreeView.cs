75ab82354b15c11ccff15d043708d45ca72b2396
Kazuki Ota <kaota@microsoft.com>
2019-10-25 11:39:08 +0900

    fix

---
+++ WpfApp/Wrapper/UWPItemsTreeView.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Toolkit.Wpf.UI.XamlHost;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WpfApp.Views;
+using UWPControl = UWPApp.Controls.UwpItemsTreeView;
+
+namespace WpfApp.Wrapper
+{
+    public class UWPItemsTreeView : WindowsXamlHostBase
+    {
+        public event EventHandler<TreeViewSelectionChangedEventArgs>? SelectionChanged;
+        public UWPItemsTreeView() : base(typeof(UWPControl).FullName)
+        {
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            Bind(nameof(DataContext), DataContextProperty, UWPControl.DataContextProperty);
+            base.OnInitialized(e);
+        }
+
+        protected override void OnChildChanged()
+        {
+            if (ChildInternal is UWPControl c)
+            {
+                c.SelectionChanged += ChildInternal_SelectionChanged;
+            }
+
+            base.OnChildChanged();
+        }
+
+        private void ChildInternal_SelectionChanged(object? sender, UWPApp.Controls.TreeViewSelectionChangedEventArgs e)
+        {
+            SelectionChanged?.Invoke(this, new TreeViewSelectionChangedEventArgs(e.SelectedItem));
+        }
+    }
+}
---
+++ WpfApp/Wrapper/UwpContentItemView.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Toolkit.Wpf.UI.XamlHost;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UWPControl = UWPApp.Controls.UwpContentItemView;
+
+namespace WpfApp.Wrapper
+{
+    public class UwpContentItemView : WindowsXamlHostBase
+    {
+        public UwpContentItemView() : base(typeof(UWPControl).FullName)
+        {
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            Bind(nameof(DataContext), DataContextProperty, UWPControl.DataContextProperty);
+            base.OnInitialized(e);
+        }
+    }
+}
